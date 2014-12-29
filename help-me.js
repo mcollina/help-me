87c7f94fd1fda04788ad35ff3060e5e4716dec2f
Matteo Collina <hello@matteocollina.com>
2014-12-29 11:52:57 +0100

    Added support for abbreviations and folders.

--- help-me.js
+++ help-me.js
@@ -1,11 +1,14 @@
 
-var fs = require('fs')
-  , path = require('path')
-  , PassThrough = require('readable-stream').PassThrough
-  , xtend = require('xtend')
-  , defaults = {
+var fs          = require('fs')
+  , path        = require('path')
+  , through     = require('through2')
+  , globStream  = require('glob-stream')
+  , concat      = require('callback-stream')
+  , xtend       = require('xtend')
+  , defaults    = {
         dir: path.join(path.dirname(require.main.filename), 'doc')
       , ext: '.txt'
+      , help: 'help'
     }
   , pump = require('pump')
 
@@ -28,20 +31,50 @@ function helpMe(opts) {
   }
 
   function createStream(args) {
-    var out         = new PassThrough()
-    var toStream    = toPath('help')
-
     if (typeof args === 'string') {
-      toStream = toPath(args)
-    } else if (args && args.length > 0) {
-      toStream = toPath(args[0])
+      args = args.split(' ')
+    } else if (!args || args.length === 0) {
+      args = [opts.help]
     }
 
-    return fs.createReadStream(toStream)
-      .on('error', function(err) {
-        out.emit('error', err)
+    var out         = through()
+      , gs          = globStream.create([opts.dir + '/**/*' + opts.ext])
+      , re          = new RegExp(args.map(function(arg) {
+          return arg + '[a-zA-Z0-9]*'
+        }).join('[ /]+'))
+
+    gs.pipe(concat({ objectMode: true }, function(err, files) {
+      if (err) return out.emit('error', err)
+
+      files = files.map(function(file) {
+        file.relative = file.path.replace(file.base, '')
+        return file
+      }).filter(function(file) {
+        return file.relative.match(re)
       })
-      .pipe(out)
+
+      if (files.length === 0) {
+        return out.emit('error', new Error('no such help file'))
+      } else if (files.length > 1) {
+        out.write('There are ' + files.length + ' help pages ')
+        out.write('that matches the given request, please disambiguate:\n')
+        files.forEach(function(file) {
+          out.write('  * ')
+          out.write(file.relative.replace(opts.ext, ''))
+          out.write('\n')
+        })
+        out.end()
+        return
+      }
+
+      fs.createReadStream(files[0].path)
+        .on('error', function(err) {
+          out.emit('error', err)
+        })
+        .pipe(out)
+    }));
+
+    return out
   }
 
   function toStdout(args) {
--- test.js
+++ test.js
@@ -8,7 +8,7 @@ test('show the doc/help.txt from the require.main folder if no options are passe
   t.plan(2)
 
   helpMe()
-    .createHelpStream()
+    .createStream()
     .pipe(concat(function(data) {
       fs.readFile('./doc/help.txt', function(err, expected) {
         t.error(err)
@@ -22,7 +22,7 @@ test('show a generic help.txt from a folder to a stream', function(t) {
 
   helpMe({
       dir: 'fixture/basic'
-  }).createHelpStream()
+  }).createStream()
     .pipe(concat(function(data) {
       fs.readFile('fixture/basic/help.txt', function(err, expected) {
         t.error(err)
@@ -36,7 +36,7 @@ test('custom help command with an array', function(t) {
 
   helpMe({
       dir: 'fixture/basic'
-  }).createHelpStream(['hello'])
+  }).createStream(['hello'])
     .pipe(concat(function(data) {
       fs.readFile('fixture/basic/hello.txt', function(err, expected) {
         t.error(err)
@@ -49,11 +49,11 @@ test('custom help command without an ext', function(t) {
   t.plan(2)
 
   helpMe({
-      dir: 'fixture/basic'
+      dir: 'fixture/no-ext'
     , ext: ''
-  }).createHelpStream(['hello'])
+  }).createStream(['hello'])
     .pipe(concat(function(data) {
-      fs.readFile('fixture/basic/hello', function(err, expected) {
+      fs.readFile('fixture/no-ext/hello', function(err, expected) {
         t.error(err)
         t.equal(data.toString(), expected.toString())
       })
@@ -65,7 +65,7 @@ test('custom help command with a string', function(t) {
 
   helpMe({
       dir: 'fixture/basic'
-  }).createHelpStream('hello')
+  }).createStream('hello')
     .pipe(concat(function(data) {
       fs.readFile('fixture/basic/hello.txt', function(err, expected) {
         t.error(err)
@@ -79,9 +79,114 @@ test('missing help file', function(t) {
 
   helpMe({
       dir: 'fixture/basic'
-  }).createHelpStream('abcde')
+  }).createStream('abcde')
     .on('error', function(err) {
-      t.equal(err.code, 'ENOENT')
+      t.equal(err.message, 'no such help file')
     })
     .resume()
+})
+
+test('custom help command with an array', function(t) {
+  var helper = helpMe({
+      dir: 'fixture/shortnames'
+  })
+
+  t.test('abbreviates two words in one', function(t) {
+    t.plan(2)
+
+    helper
+      .createStream(['world'])
+      .pipe(concat(function(data) {
+        fs.readFile('fixture/shortnames/hello world.txt', function(err, expected) {
+          t.error(err)
+          t.equal(data.toString(), expected.toString())
+        })
+      }))
+  })
+
+  t.test('abbreviates three words in two', function(t) {
+    t.plan(2)
+
+    helper
+      .createStream(['abcde', 'fghi'])
+      .pipe(concat(function(data) {
+        fs.readFile('fixture/shortnames/abcde fghi lmno.txt', function(err, expected) {
+          t.error(err)
+          t.equal(data.toString(), expected.toString())
+        })
+      }))
+  })
+
+  t.test('abbreviates a word', function(t) {
+    t.plan(2)
+
+    helper
+      .createStream(['abc', 'fg'])
+      .pipe(concat(function(data) {
+        fs.readFile('fixture/shortnames/abcde fghi lmno.txt', function(err, expected) {
+          t.error(err)
+          t.equal(data.toString(), expected.toString())
+        })
+      }))
+  })
+
+  t.test('abbreviates a word using strings', function(t) {
+    t.plan(2)
+
+    helper
+      .createStream('abc fg')
+      .pipe(concat(function(data) {
+        fs.readFile('fixture/shortnames/abcde fghi lmno.txt', function(err, expected) {
+          t.error(err)
+          t.equal(data.toString(), expected.toString())
+        })
+      }))
+  })
+
+  t.test('print a disambiguation', function(t) {
+    t.plan(1)
+
+    var expected = '' +
+      'There are 2 help pages that matches the given request, please disambiguate:\n' +
+      '  * abcde fghi lmno\n' +
+      '  * abcde hello\n'
+
+    helper
+      .createStream(['abc'])
+      .pipe(concat({ encoding: 'string' }, function(data) {
+        t.equal(data, expected)
+      }))
+  })
+})
+
+test('support for help files organized in folders', function(t) {
+  var helper = helpMe({
+    dir: 'fixture/dir'
+  })
+
+  t.test('passing an array', function(t) {
+    t.plan(2)
+
+    helper
+      .createStream(['a', 'b'])
+      .pipe(concat(function(data) {
+        fs.readFile('fixture/dir/a/b.txt', function(err, expected) {
+          t.error(err)
+          t.equal(data.toString(), expected.toString())
+        })
+      }))
+  })
+
+  t.test('passing a string', function(t) {
+    t.plan(2)
+
+    helper
+      .createStream('a b')
+      .pipe(concat(function(data) {
+        fs.readFile('fixture/dir/a/b.txt', function(err, expected) {
+          t.error(err)
+          t.equal(data.toString(), expected.toString())
+        })
+      }))
+  })
 })
